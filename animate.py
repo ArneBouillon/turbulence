74526b964116fec40745a06a0ef9a2e25140b792
Sem Viroux <sem.viroux@hotmail.com>
2020-10-23 17:25:04 +0200

    Added simulation output for L_d/T_0=4.
    - added archive folder with .mp4 output. (Simulation output data still available on my hard drive)
    - name of folder R25000_L4_20201023 > Reynolds _ L_d/T_0 _ date
    - small change in animate.py (added variable to easily change data to plot

--- animate.py
+++ animate.py
@@ -7,10 +7,11 @@ from matplotlib import rc_context
 
 ts = yt.load('output/turbulence_*.dat')
 
-plot = yt.SlicePlot(ts[0], 'z', 'm1')
-plot.set_zlim('m1', -1e-5, 5e0)
+data = 'm1'
+plot = yt.SlicePlot(ts[0], 'z', data)
+plot.set_zlim(data, -1e-5, 5e0)
 
-fig = plot.plots['m1'].figure
+fig = plot.plots[data].figure
 
 def animate(i):
     ds = ts[i]
@@ -19,5 +20,5 @@ def animate(i):
 animation = FuncAnimation(fig, animate, frames=len(ts))
 
 gif_writer = PillowWriter(fps=30)
-animation.save('animation.gif', writer=gif_writer)
+animation.save(data + '_animation.gif', writer=gif_writer)
 
