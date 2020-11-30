1714df6d7d4543e416ba20a19251e23923d76c3a
Sem Viroux <sem.viroux@hotmail.com>
2020-11-30 15:22:49 +0100

    convergence test on grid sizes plot

--- plotting/plot_ref_sizes.m
+++ plotting/plot_ref_sizes.m
@@ -4,17 +4,17 @@ blue = [ 0.11,0.55,0.69 ];
 sizes = [ 50 100 200 400 800 ];
 timings = [ 2*60+19 5*60+7 20*60+55 1*3600+8*60+1 4*3600+28*60+29 ];
 
-figure(1);
+figure(1); clf;
 loglog(sizes, timings, "-x", "Color", dark_blue, "LineWidth", 1.5, "MarkerSize", 12);
 hold on
-loglog(sizes, sizes/sizes(1)*timings(1))
-loglog(sizes, sizes.^2/sizes(1).^2*timings(1))
+loglog([sizes 1600], [sizes 1600]/sizes(1)*timings(1),"--",  "Color", blue, "LineWidth", 1.1)
+loglog(sizes, sizes.^2/sizes(1).^2*timings(1),"--",  "Color", blue,"LineWidth", 1.1)
 xlabel("Number of grid points in the X and Y directions", "interpreter", "Latex");
 ylabel("Computing time [s]", "interpreter", "Latex");
-ylim([0 max(timings)*1.2]);
+ylim([min(timings)/1.4 max(timings)*1.4]);
 xlim([min(sizes)/1.2 max(sizes)*1.2]);
 xticks(sizes);
-xticklabels({'$$\begin{array}{c} 50 \\ \end{array}$$', '$$\begin{array}{c} 100 \\ \end{array}$$', '$$\begin{array}{c} 200 \\  \end{array}$$', '$$\begin{array}{c} 400 \\ \phantom{s} \end{array}$$','$$\begin{array}{c} 800 \\ \end{array}$$'}); set(gca,'TickLabelInterpreter','latex');
+%xticklabels({'$$\begin{array}{c} 50 \\ \end{array}$$', '$$\begin{array}{c} 100 \\ \end{array}$$', '$$\begin{array}{c} 200 \\  \end{array}$$', '$$\begin{array}{c} 400 \\ \phantom{s} \end{array}$$','$$\begin{array}{c} 800 \\ \end{array}$$'}); set(gca,'TickLabelInterpreter','latex');
 %title("Strong scaling", "interpreter", "Latex")
 set(gca, 'FontName', 'Times New Roman')
 set(gca,'XMinorTick','off')
